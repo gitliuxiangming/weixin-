116ef9eeada3cd5d6591bf292338bbc665780b06
刘向明 <719327306@qq.com>
2018-09-21 17:38:54 +0800

    9.21

--- 02-test/data/db.js
+++ 02-test/data/db.js
@@ -7,13 +7,16 @@ var articles = [
       content:'不听不听 王八念经',
       avatar: '/images/avatar/touxiang1.jpg',
       bankground: '../../../images/article/article-detail-1.jpg',
-      music:'../../../images/article/article-detail-music.jpg',
       date: '2108-09-19',
       image: '../../images/avatar/1.jpg',
       content: '我们这一代人，人称老三届',
       guanzhu: 1,
       view: 88,
-      articleId:0
+      articleId:0,
+      music:{
+      src:'http://oxoxtpvtn.bkt.clouddn.com/%E8%96%9B%E4%B9%8B%E8%B0%A6%20-%20%E6%BC%94%E5%91%98.mp3',
+      title: '演员',       coverImgUrl:'http://oxoxtpvtn.bkt.clouddn.com/%E8%96%9B%E4%B9%8B%E8%B0%A6%E6%BC%94%E5%91%98.jpg'
+      }
     },
     {
       title: '文章二',
@@ -22,13 +25,17 @@ var articles = [
       content: '不听不听 王八念经',
       avatar: '/images/avatar/touxiang2.jpg',
       bankground: '../../../images/article/article-detail-2.jpg',
-      music: '../../../images/article/article-detail-music.jpg',
       date: '2108-09-10',
       image: '../../images/avatar/2.jpg',
       content: '第二篇文章',
       guanzhu: 8,
       view: 58,
-      articleId: 1
+      articleId: 1,
+      music: {
+        src: 'http://oxoxtpvtn.bkt.clouddn.com/%E6%9D%8E%E5%81%A5%E3%80%81%E5%AD%99%E4%BF%AA%20-%20%E9%A3%8E%E5%90%B9%E9%BA%A6%E6%B5%AA%20%28Live%29.mp3',
+        title: '风吹麦浪',
+        coverImgUrl: 'http://oxoxtpvtn.bkt.clouddn.com/%E9%A3%8E%E5%90%B9%E9%BA%A6%E6%B5%AA.jpg'
+      }
     }]
   
 
--- 02-test/pages/article/article-detail/article-detail.js
+++ 02-test/pages/article/article-detail/article-detail.js
@@ -5,14 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    isPlayMusic:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('sd',options)
     var articleId = options.articleId;
     var article = articles[articleId];
     this.data.articleId = articleId;
@@ -32,14 +31,30 @@ Page({
     this.setData({
       currentIsCollented: currentIsCollented
     })
+    /**
+     * 监听音乐播放
+     */
+    var _this = this;
+    var music = wx.getBackgroundAudioManager();
+    music.onPlay(function(){
+      _this.setData({
+        isPlayMusic: true
+      })
+    })
+    music.onPause(function () {
+      _this.setData({
+        isPlayMusic: false
+      })
+    })
   },
   /**
    * 处理收藏
    */
   tapCollection: function () {
+    var _this = this;
     var articlesCollection = wx.getStorageSync("articles_collection");
     var currentIsCollented = articlesCollection[this.data.articleId];
-    console.log(currentIsCollented);
+    // console.log(currentIsCollented);
     //改变storage里面的值
     articlesCollection[this.data.articleId] = !currentIsCollented;
     wx.setStorageSync("articles_collection", articlesCollection);
@@ -47,5 +62,65 @@ Page({
     this.setData({
       currentIsCollented: !currentIsCollented
     })
+    
+    wx.showToast({
+      title: currentIsCollented ? '取消收藏' :'收藏成功',
+    })
+    
+    /*
+    wx.showModal({
+      title: currentIsCollented ? '取消收藏' : '收藏成功',
+      success:function(res){
+        if(res.confirm){
+          articlesCollection[_this.data.articleId] = !currentIsCollented;
+          wx.setStorageSync("articles_collection", articlesCollection);
+          //改变页面渲染
+          _this.setData({
+            currentIsCollented: !currentIsCollented
+          })
+        }
+      }
+    })
+    */
   },
+  /**
+   * 处理分享
+   */
+  tapShare:function(){
+    var itemList = ['分享到微博', '分享到QQ', '分享到朋友圈',];
+    wx.showActionSheet({
+      itemList: itemList,
+      success:function(res){
+        var item = itemList[res.tapIndex];
+        wx.showToast({
+          title: item+'成功',
+        })
+      }
+    })
+  },
+  /**
+   * 处理音乐播放
+   */
+  tapMusic:function(){
+    var music = wx.getBackgroundAudioManager();
+    var isPlayMusic = this.data.isPlayMusic;
+    if(isPlayMusic){
+      music.pause()
+      this.setData({
+        isPlayMusic:false
+      })
+     
+    }else{
+      music.src = this.data.music.src;
+      music.title = this.data.music.title;
+      music.coverImgUrl = this.data.music.coverImgUrl;
+      music.play()
+      this.setData({
+        isPlayMusic: true
+      })
+      
+    }
+
+
+  }
 })
